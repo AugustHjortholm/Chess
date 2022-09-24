325cd83c7860fd299c341966655c8c595cafdf63
AugustHjortholm <s215800@dtu.dk>
2022-09-24 13:29:31 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        GameController gameController = new GameController();
-        gameController.runGame();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.next();
+            if(command.equals("PLAY")) {
+                GameController gameController = new GameController();
+                gameController.runGame();
+            }
+            else if (command.equals("EXIT")){
+                break;
+            }
+        }
     }
 }
